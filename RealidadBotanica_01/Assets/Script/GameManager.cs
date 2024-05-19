a718c0fa347eab8893b7e0112c98c2dc2c64f0eb
Nadia Gonzalez <yologonzalezv@gmail.com>
2024-05-19 01:30:36 -0600

    Script One

---
+++ RealidadBotanica_01/Assets/Script/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OneMainMenu;
+    public event Action OnitemsMenu;
+    public event Action OnARposition;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject );
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        OnitemsMenu?.Invoke();
+        Debug.Log("Main Menu Activated");
+    }
+    public void ItemsMenu()
+    {
+        OnitemsMenu?.Invoke();
+        Debug.Log("Items Menu Activated");
+    }
+    public void ARPosition()
+    { 
+        OnARposition?.Invoke();
+        Debug.Log("AR Position Activated");
+    }
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
