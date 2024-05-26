ea7786cb56e3799d583e9e7394885a008f341066
Nadia Gonzalez <yologonzalezv@gmail.com>
2024-05-25 19:07:12 -0600

    PrimerasPruebas

--- RealidadBotanica_01/Assets/Script/UIManager.cs
+++ RealidadBotanica_01/Assets/Script/UIManager.cs
@@ -48,7 +48,7 @@ public class UIManager : MonoBehaviour
     {
         mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
         mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-        mainMenuCanvas.transform.GetChild(2).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
+        //mainMenuCanvas.transform.GetChild(2).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
 
         itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
